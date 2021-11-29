0fb16a458842376d7d39519136a87a1bafce6eba
Ying Wit Mon Win <yingwitmon.win@gmail.com>
2021-11-29 20:21:23 +0630

    Adds another apiKey

--- src/constants/apiSettings.js
+++ src/constants/apiSettings.js
@@ -1,6 +1,5 @@
 export const API_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us';
 export const API_SOURCES_URL = 'https://newsapi.org/v2/sources?';
 export const API_SEARCH_URL = 'https://newsapi.org/v2/top-headlines?q=';
-export const API_KEY = '099148be22804e849a0c6fe022b7cf5e';
-
-// export const API_KEY = 'HELLO';
+// export const API_KEY = '099148be22804e849a0c6fe022b7cf5e';
+export const API_KEY = 'e075c02da21a4772b113bc85e08821e9';
